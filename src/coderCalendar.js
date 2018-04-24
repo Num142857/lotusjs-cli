9c0826c110546763f8bf4406991778ada8ea3fe5
杨帆 <incomparable9527@foxmail.com>
2018-04-24 17:24:55 +0800

    优化代码

--- src/coderCalendar.js
+++ src/coderCalendar.js
@@ -2,6 +2,8 @@
 const moment = require("moment");
 var chalk = require("chalk")
 moment.locale('zh-cn');
+let baseNumber = moment().dayOfYear() * 666 * 888;
+let tools = ["VS Code写程序", "MSOffice写文档", "记事本写程序", "Windows10", "Linux", "MacOS", "IE", "Android设备", "iOS设备"];
 let activities = [
     { name: "写单元测试", good: "写单元测试将减少出错", bad: "写单元测试会降低你的开发效率" },
     { name: "洗澡", good: "你几天没洗澡了？", bad: "会把设计方面的灵感洗掉", weekend: true },
@@ -9,7 +11,7 @@ let activities = [
     { name: "抽烟", good: "抽烟有利于提神，增加思维敏捷", bad: "除非你活够了，死得早点没关系", weekend: true },
     { name: "白天上线", good: "今天白天上线是安全的", bad: "可能导致灾难性后果" },
     { name: "重构", good: "代码质量得到提高", bad: "你很有可能会陷入泥潭" },
-    { name: "使用%t", good: "你看起来更有品位", bad: "别人会觉得你在装逼" },
+    { name: `使用${tools[baseNumber % baseNumber.length]}`, good: "你看起来更有品位", bad: "别人会觉得你在装逼" },
     { name: "跳槽", good: "该放手时就放手", bad: "鉴于当前的经济形势，你的下一份工作未必比现在强" },
     { name: "招人", good: "你面前这位有成为牛人的潜质", bad: "这人会写程序吗？" },
     { name: "面试", good: "面试官今天心情很好", bad: "面试官不爽，会拿你出气" },
@@ -19,8 +21,7 @@ let activities = [
     { name: "晚上加班", good: "晚上是程序员精神最好的时候", bad: "他妈的晚上又要加班", weekend: true },
     { name: "在妹子面前吹牛", good: "改善你矮穷挫的形象", bad: "会被识破", weekend: true },
     { name: "浏览成人网站", good: "重拾对生活的信心", bad: "你会心神不宁", weekend: true },
-    { name: "命名变量%v", good: "", bad: "" },
-    { name: "写超过%l行的方法", good: "你的代码组织的很好，长一点没关系", bad: "你的代码将混乱不堪，你自己都看不懂" },
+    { name: `写超过501行的方法`, good: "你的代码组织的很好，长一点没关系", bad: "你的代码将混乱不堪，你自己都看不懂" },
     { name: "提交代码", good: "遇到冲突的几率是最低的", bad: "你遇到的一大堆冲突会让你觉得自己是不是时间穿越了" },
     { name: "代码复审", good: "发现重要问题的几率大大增加", bad: "你什么问题都发现不了，白白浪费时间" },
     { name: "开会", good: "写代码之余放松一下打个盹，有益健康", bad: "小心被扣屎盆子背黑锅" },
@@ -34,13 +35,17 @@ let activities = [
 ]
 let weeks = ["日", "一", "二", "三", "四", "五", "六"]
 let directions = ["北方", "东北方", "东方", "东南方", "南方", "西南方", "西方", "西北方"]
-let tools = ["Eclipse写程序", "MSOffice写文档", "记事本写程序", "Windows8", "Linux", "MacOS", "IE", "Android设备", "iOS设备"];
 let drinks = ["水", "茶", "红茶", "绿茶", "咖啡", "奶茶", "可乐", "牛奶", "豆奶", "果汁", "果味汽水", "苏打水", "运动饮料", "酸奶", "酒"]
 
+function RandomNumBoth(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    var num = Min + Math.round(Rand * Range); //四舍五入
+    return num;
+}
 
 module.exports = {
     init(){
-        let baseNumber = moment().dayOfYear() * 666 * 888;
         let luckyNumber = (baseNumber / 6)+""
         let indexofLucky = luckyNumber.split('');
         
