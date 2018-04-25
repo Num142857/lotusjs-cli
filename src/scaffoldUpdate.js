40cc957a4018b237c2b29566eda2b9b07b7395b2
杨帆 <incomparable9527@foxmail.com>
2018-04-25 20:45:50 +0800

    fixbug

--- src/scaffoldUpdate.js
+++ src/scaffoldUpdate.js
@@ -3,7 +3,7 @@ var spawn = require('child_process').spawn;
 const shelljs = require('shelljs');
 const download = require('download-git-repo');
 const fse = require('fs-extra')
-var fs= require('fs')
+var fs = require('fs')
 
 module.exports = {
     config: [{
@@ -11,17 +11,23 @@ module.exports = {
         name: 'do',
         message: 'What do you want to do?',
         choices: ['初始化', '更新']
-    },],
+    }, ],
     callback: async function (answers) {
         try {
             switch (answers.do) {
                 case '初始化':
                     log.info('正在初始化你的脚手架')
-                    let exist =  await fse.ensureDir(process.cwd()+"/_lotus")
-                    
-                    if(!exist){log.warn('文件夹_lotus已存在,作业停止');return}
-                    download('github:Fantasy9527/lotus-scaffold', process.cwd(), (err)=>{
-                        if (err) { log.warn(err); return}
+                    let exist = await fse.ensureDir(process.cwd() + "/_lotus")
+
+                    if (!exist) {
+                        log.warn('文件夹_lotus已存在,作业停止');
+                        return
+                    }
+                    download('github:Fantasy9527/lotus-scaffold', process.cwd(), (err) => {
+                        if (err) {
+                            log.warn(err);
+                            return
+                        }
                         this.generatePackage()
                         log.info('脚手架初始化完成')
                     })
@@ -31,22 +37,25 @@ module.exports = {
                     let questions = await inquirer.prompt([{
                         type: 'list',
                         name: 'do',
-                        message: `更新会直接覆盖掉原脚手架代码.如果之前有过修改,请做好备份.
+                        message: `更新会直接覆盖掉原脚手架核心代码.如果之前有过修改,请做好备份.
   现在是否更新你的脚手架`,
                         choices: ['是', '否']
                     }]);
                     if (questions === '否') return;
                     log.info('正在更新你的脚手架核心代码')
-                    let temp_dir = process.cwd()+"/temp_lotus";
-                    let target_dir = process.cwd()+"/_lotus";
-                    download('github:Fantasy9527/lotus-scaffold', temp_dir,async  (err)=> {
-                        if (err) { log.warn(err); return }
+                    let temp_dir = process.cwd() + "/temp_lotus";
+                    let target_dir = process.cwd() + "/_lotus";
+                    download('github:Fantasy9527/lotus-scaffold', temp_dir, async (err) => {
+                        if (err) {
+                            log.warn(err);
+                            return
+                        }
 
-                       let copy_bin =  await fse.copy(temp_dir+"/_lotus/bin", target_dir+"/bin")
-                       
-                       this.generatePackage()
-                       await fse.remove(temp_dir)
-                       log.info('脚手架核心代码更新完成')
+                        let copy_bin = await fse.copy(temp_dir + "/_lotus/bin", target_dir + "/bin")
+
+                        this.generatePackage()
+                        await fse.remove(temp_dir)
+                        log.info('脚手架核心代码更新完成')
                     })
                     break;
             }
@@ -55,27 +64,35 @@ module.exports = {
         }
 
     },
-   async generatePackage(){
+    async generatePackage() {
         let packagePath = `${process.cwd()}/package.json`
         let pathArr = process.cwd().split('/')
-        let appName = pathArr[pathArr.length-1]
+        let appName = pathArr[pathArr.length - 1]
         let packageExist = fs.existsSync(packagePath)
-
-          if(packageExist){
+        // console.log(packageExist)
+        if (packageExist) {
+            //修改文件
             let appinfo = await fse.readJson(packagePath)
             appinfo.bin = {}
             appinfo.name = appName
-            appinfo.bin[appName] ="_lotus/bin/index.js"
-            await fse.writeJson(packagePath, appinfo,{spaces:4})
-          }else{
+            appinfo.scaffold = appName
+            appinfo.bin[appName] = "_lotus/bin/index.js"
+            await fse.writeJson(packagePath, appinfo, {
+                spaces: 4
+            })
+        } else {
+            //重新生成
             let appinfo = {
                 "name": appName,
+                "scaffold": appName,
                 "version": "0.0.1",
-                "bin":{}
+                "bin": {}
             }
-            appinfo.bin[appName] ="_lotus/bin/index.js"  
-            await fse.writeJson(packagePath, appinfo,{spaces:4})
-          }
+            appinfo.bin[appName] = "_lotus/bin/index.js"
+            await fse.writeJson(packagePath, appinfo, {
+                spaces: 4
+            })
+        }
 
     }
 }
